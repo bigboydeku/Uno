e94777568a95e2706d0e482a7faaea4598d5c272
Denis Nadarevic <nadarevd@cs.uwindsor.ca>
2019-09-05 18:26:23 -0400

    working on card deck

--- src/clientside/Card.java
+++ src/clientside/Card.java
@@ -1,14 +1,8 @@
 import java.util.*;
-import java.io.*;
-import java.net.*;
 
-public PlayerDeck implements Serializable {
-  private ServerSideConnection ssc;
+public class Card {
 
-  public PlayerDeck() {
-    ArrayList<> cards = new ArrayList<>();
 
-  }
 
   private enum CARD_NUMBER_STATE { 0,1,2,3,4,5,6,7,8,9;
     public static CARD_NUMBER_STATE getRandom() {
@@ -16,8 +10,11 @@ public PlayerDeck implements Serializable {
       int i = random.nextInt(values().length);
       return values()[i];
     }
+
   }
 
-  public static ArrayList<>
 
+  public static void main(String [] args ) {
+
+  }
 }
